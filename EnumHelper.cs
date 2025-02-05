b1296f4b20d60569765fcd8f78378691f4840276
Filipe Carneiro <fcarneiro@doutorpc.net>
2025-02-05 15:34:51 +0000

    Add project files.

---
+++ EnumHelper.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace Extensions
+{
+    public static class EnumHelper
+    {
+        public static string? GetDisplayName(Enum? enumValue)
+        {
+            if (enumValue == null)
+                return null;
+
+            return enumValue?.GetType()?
+           .GetMember(enumValue.ToString())?[0]?
+           .GetCustomAttribute<DisplayAttribute>()?
+           .Name;
+        }
+
+        public static string? GetDisplayDescription(Enum? enumValue)
+        {
+            if (enumValue == null)
+                return null;
+
+            return enumValue?.GetType()?
+           .GetMember(enumValue.ToString())?[0]?
+           .GetCustomAttribute<DisplayAttribute>()?
+           .Description;
+        }
+    }
+}
---
+++ UrlHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace Extensions
+{
+    public static class UrlHelper
+    {
+        public static string GetHost(string url)
+        {
+            Uri uri = new Uri(url);
+            return uri.Host;
+        }
+    }
+}
